19deaad80f7906a014d9dfa24b6dead2e0334bce
Ali <ali.naqvi.dev@gmail.com>
2022-07-31 13:11:40 -0500

    env

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, GuildMember, Intents } = require("discord.js");
-const { Player, QueryType } = require("discord-player");
-const config = require("./config.json");
-
-const client = new Client({
-  intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
-});
-client.login(config.token);
