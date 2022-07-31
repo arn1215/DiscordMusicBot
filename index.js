21a180ff7ac37508177e926c803381d3ace7e52d
Ali <ali.naqvi.dev@gmail.com>
2022-07-31 13:50:07 -0500

    bot is workinggit add .!

---
+++ index.js
@@ -0,0 +1,33 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+
+
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
+
+
